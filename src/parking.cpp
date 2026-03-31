6aa6bb41f89679dd01d9da24167a8a853a40f97f
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2026-03-31 17:15:42 -0400

    shortened names,ParkingTicket::report,f operator<<
    changed some data member names to more concise counterparts.
    
    implemented the ParkingTicket report function
    implemented the friend function operator<< overload for ParkingTicket

--- src/parking.cpp
+++ src/parking.cpp
@@ -1,26 +1,26 @@
 #include "parking.h"
 #include <string>
+#include <sstream>
 #include <iostream>
 #include <optional>
-
 using namespace std;
 
 ParkedCar::ParkedCar(
   string make, string model, Color color, 
   unsigned license_num, unsigned minutes_parked):
     make_m(make), model_m(model), color_m(color),
-    license_num_m(license_num), minutes_parked_m(minutes_parked) {}
+    license_num_m(license_num), mins_parked_m(minutes_parked) {}
 
 ParkedCar& ParkedCar::operator++() 
 {
-  ++minutes_parked_m;
+  ++mins_parked_m;
   return *this;
 }
 
 ParkedCar ParkedCar::operator++(int)
 {
   ParkedCar copy { *this };
-  ++minutes_parked_m;
+  ++mins_parked_m;
   return copy;
 }
 
@@ -40,11 +40,12 @@ ParkingTicket::ParkingTicket(
   ParkedCar &ticketed, ParkingMeter &meter, PoliceOfficer &popo):
     ticketed_m(ticketed), meter_m(meter), popo_m(popo) {}
 
-unsigned ParkingTicket::getFine(unsigned mins_parked, unsigned mins_purchased) 
+unsigned ParkingTicket::getFine() const
 {
   // only gets called if minutes_parked > minutes_purchased
   unsigned fine {};
-  unsigned mins_violated { mins_parked - mins_purchased };
+  unsigned mins_violated 
+  { ticketed_m.getMinutesParked() - meter_m.getMinutesPurchased() };
 
   // add ten for any partial hours not handled by the rounded division
   if (mins_violated % 60) fine += 10; 
@@ -53,4 +54,35 @@ unsigned ParkingTicket::getFine(unsigned mins_parked, unsigned mins_purchased)
   fine += mins_violated / 60 * 25;
 
   return fine;
+}
+
+string ParkingTicket::report() const 
+{
+  ostringstream report {};
+
+  // generate the report
+  report << "Make: " << ticketed_m.getMake() << '\n'
+    << "Model: " << ticketed_m.getModel() << '\n'
+    << "Color: " << ticketed_m.getColor() << '\n'
+    << "License Number: " << ticketed_m.getLicenseNum() << '\n'
+    << "Minutes Parked: " << ticketed_m.getMinutesParked() << '\n'
+    << "Minutes Purchased: " << meter_m.getMinutesPurchased() << '\n'
+    << "Fine: " << this->getFine() << '\n'
+    << "Officer Name: " << popo_m.getName() << '\n'
+    << "Officer Badge Num: " << popo_m.getBadgeNum();
+
+  // convert to string and return
+  return report.str();
+}
+
+ostream& operator<<(ostream &ostream, const ParkingTicket &ticket) 
+{
+  // check for and if present clear failure state on ostream
+  if (ostream) ostream.clear();
+
+  // insert the report into the ostream 
+  ostream << ticket.report();
+
+  // return for chaining
+  return ostream;
 }
