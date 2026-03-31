cedc36d488e16385ccbf5c7da95c9aa3e99f1810
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2026-03-31 15:12:41 -0400

    def Color,inlined getters and setters
    getters and setters and signatures

--- include/Parking.h
+++ include/Parking.h
@@ -1,8 +1,10 @@
 #include <string>
 #include <optional>
 
-enum Color {
+class ParkingTicket;
 
+enum Color {
+  RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, BLACK, WHITE, TAN,
 };
 
 class ParkedCar
@@ -11,28 +13,60 @@ class ParkedCar
   Color color;
   unsigned license_num;
   unsigned minutes_parked;
+public:
+  ParkedCar(
+      std::string make, std::string model, Color color, 
+      unsigned license_num, unsigned minutes_parked);
+
+  inline std::string getMake() const 
+  { return make; }
+
+  inline std::string getModel() const
+  { return model; }
+
+  inline Color getColor() const
+  { return color; }
+
+  inline unsigned getLicenseNum() const
+  { return license_num; }
+
+  inline unsigned getMinutedParked() const
+  { return minutes_parked; }
+
+  inline void setMinutesParked(unsigned minutes)
+  { minutes_parked = minutes; }
+
+  ParkedCar& operator++();
+  ParkedCar& operator++(int);
 };
 
 class ParkingMeter
 {
   unsigned minutes_purchased;
-};
+public:
+  ParkingMeter(unsigned mins = 30);
 
-class ParkingTicket;
+  inline unsigned getMinutesPurchased(void) 
+  { return minutes_purchased; }
+
+  inline void setMinutesPurchased(unsigned mins) 
+  { minutes_purchased = mins; }
+};
 
 class PoliceOfficer 
 {
   std::string name;
   unsigned badge_num;
-
 public:
-  inline unsigned getBadgeNum() const { return badge_num; }
-  std::string getName() const { return name; }
+  inline unsigned getBadgeNum() const
+  { return badge_num; }
+
+  std::string getName() const 
+  { return name; }
 
   /// returns an optional containing a ParkingTicket if the car needs to be 
-  /// ticketed, if otherwise an empty optional is returned.
+  /// ticketed, otherwise an empty optional is returned.
   std::optional<ParkingTicket> inspectParking();
-
 private:
   /// returns true if a Car is violating the parking laws.
   bool isViolating() const;
@@ -43,4 +77,9 @@ class ParkingTicket
   ParkedCar ticketed;
   ParkingMeter meter;
   PoliceOfficer popo;
+public:
+  /// Generates a report 
+  std::string report() const;
+private:
+  unsigned getFine() const;
 };
