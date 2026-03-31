17d31949b22ddcd0de4d4da129b090a4e028e149
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2026-03-31 17:54:56 -0400

    test messages and signatures, no test fn def's yet
    also wrote the calls into the main function. All that's left is
    bugfixing and test implementation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,42 @@
+#include "parking.h"
 #include <iostream>
 
+using namespace std;
+
+const char *PASS { "PASS" };
+const char *OBJ_CREATION_TEST { "Test (Basic Object Creation): " };
+const char *LEGAL_PARKING_TEST { "Test (Legal Parking): " };
+const char *ILLEGAL_PARKING { "Test (Illegal Parking): " };
+const char *MULTIPLE_CARS { "Test (Multiple Cars): " };
+const char *FINE_TEST { "Test (Fine Calculation Verification): " };
+
+void objCreationTest(void);
+
+void legalParkingTest(void);
+
+void illegalParkingTest(void);
+
+void multipleCarsTest(void);
+
+void fineCalculationTest(void);
+
 int main(void) {
-  std::cout << "Hello, Amos!" << std::endl;
+  objCreationTest(); 
+  cout << '\n';
+
+  legalParkingTest();
+  cout << '\n';
+
+  illegalParkingTest();
+  cout << '\n';
+
+  fineCalculationTest();
+  cout << endl;
 
   return 0;
+}
+
+void objCreationTest(void)
+{
+  
 }
